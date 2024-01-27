f1112a196b568cf4ad6c2f2db1b6e68283b6865a
MLDODSON <paola@dominicansoul.com>
2024-01-27 11:57:23 -0600

    Branch 1
    Branch 1 - Lorem Ipsum generator project setup.

---
+++ lorem/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import '@fontsource-variable/open-sans';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
